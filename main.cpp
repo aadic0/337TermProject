f44375a529bf3e1443223231f7f8e7ecc067d23f
aadic0 <126915861+aadic0@users.noreply.github.com>
2023-11-26 20:28:39 -0700

    Initial Commit
    Added completed header file and incomplete main function

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <ios>
+#include <fstream>
+#include <cstdlib>
+#include <istream>
+#include <ostream>
+#include "main.h"
+
+using namespace std;
+
+int main(){
+
+
+
+
+    return 0;
+}
---
+++ main.h
@@ -0,0 +1,108 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+
+class Airline{
+
+    public:
+        // constructors
+        Airline();
+        Airline(Airline& src);
+        Airline& operator=(Airline& rhs);
+        ~Airline(); 
+
+        // getter functions
+        inline std::string getAirlineName() {return AirlineName;}
+        inline std::vector<std::string> getFlightList() {return FlightList;}
+        inline int getFlightNumber() {return FlightNumber;}
+
+        // setter functions
+        inline std::string setAirlineName(std::string name) {AirlineName = name;}
+        inline std::vector<std::string> setFlightList(std::vector<std::string> list) {FlightList = list;}
+        inline int setFlightNumber(int number) {FlightNumber = number;}
+
+    private:
+        std::string AirlineName;
+        std::vector<std::string> FlightList;
+        int FlightNumber; 
+};
+
+class Flight{
+
+    public:
+        // constructors
+        Flight();
+        Flight(Flight& src);
+        Flight& operator=(Flight& rhs);
+        ~Flight();
+
+        // getter functions
+        inline int getRowsF() {return RowsF;}
+        inline int getColumnsF() {return ColumnsF;}
+        inline std::vector<std::string> getPassengerList() {return PassengerList;}
+        inline std::vector<std::vector<std::string>> getSeatMap() {return SeatMap;}
+
+        // setter functions
+        inline int setRowsF(int number) {RowsF = number;}
+        inline int setColumnsF(int number) {ColumnsF = number;}
+        inline std::vector<std::string> setPassengerList(std::vector<std::string> list) {PassengerList = list;}
+        inline std::vector<std::vector<std::string>> setSeatMap(std::vector<std::vector<std::string>> input) {SeatMap = input;}
+
+    private:
+        std::string FlightID;
+        int RowsF;
+        int ColumnsF;
+        std::vector<std::string> PassengerList;
+        std::vector<std::vector<std::string>> SeatMap; 
+
+};
+
+class Passenger{
+
+    public:
+        // constructors
+        Passenger();
+        Passenger(Passenger& src);
+        Passenger& operator=(Passenger& rhs);
+        ~Passenger();
+
+        // getter functions
+        inline int getPassengerId() {return PassengerId;}
+        inline std::string getFirstName() {return FirstName;}
+        inline std::string getLastName() {return LastName;}
+        inline int getPhoneNumber() {return PhoneNumber;}
+        inline Flight*** getPassengerSeat() {return PassengerSeat;}
+
+
+    private:
+        int PassengerId;
+        std::string FirstName;
+        std::string LastName;
+        int PhoneNumber;
+        Flight*** PassengerSeat;
+        
+};
+
+
+class Seat{
+
+    public:
+        // constructors
+        Seat();
+        Seat(Seat& src);
+        Seat& operator=(Seat& rhs);
+        ~Seat();
+
+
+    private:
+        int RowS;
+        int ColumnS;
+        int Availability;
+
+};
+
+#endif
